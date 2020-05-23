a5940530ae0cb55bd3bb823f8b0e0927efd0f8b3
Jderlich <57108571+Jderlich@users.noreply.github.com>
2020-05-23 08:23:43 +0200

    Update models.py

--- models.py
+++ models.py
@@ -3,19 +3,19 @@ import torch.nn as nn
 """
 The present python script contains the following classes:
 
-* The Extractor class used to define the feature extractor (Resnet18 without the FC layer),***(Edoardo hai detto che lo facevi quindi te lo lascio)
-* The Main_task class, used to perform the main task which a multi class classification
-* The Pre_text class which is used to predict the relative rotation between the depth and the RGB modalities
+* The FeatureExtractor class used to define the feature extractor (Resnet18 without the FC layer),***(Edoardo hai detto che lo facevi quindi te lo lascio)
+* The MainTask class, used to perform the main task which a multi class classification
+* The PreText class which is used to predict the relative rotation between the depth and the RGB modalities
 
 """
 
-class Pre_text(nn.Module):
+class PreText(nn.Module):
   """
   Pretext task
   
   """
   def __init__(self, num_classes = 4, featureMaps = 512, **kwargs):
-    super(Pre_text, self).__init__():
+    super(PreText, self).__init__():
       self.layer = nn.sequential(
             nn.Conv2d(featureMaps, 100, kernel_size = 1),
             nn.BatchNorm2d(100),
@@ -28,12 +28,12 @@ class Pre_text(nn.Module):
             nn.Linear(100, num_classes),
       )
       
-class  Main_task(nn.Module):
+class  MainTask(nn.Module):
   """
   Main classifier
   """
   def__init__(self, num_classes = 51, featureMaps = 512, **kwargs):
-    super(Main_task, self).__init__():
+    super(MainTask, self).__init__():
       self.layer = nn.Sequential(
             nn.AdaptiveAvgPool2d((7,7)),
             nn.Linear(input_size, 1000),
