aaef92ab52a8dcf27bfdb05e2ba545ddc5696bb5
Jderlich <57108571+Jderlich@users.noreply.github.com>
2020-05-26 09:18:08 +0200

    Update dataset_handler.py

--- dataset_handler.py
+++ dataset_handler.py
@@ -1,4 +1,7 @@
 import sys, os
+from torchvision.datasets import VisionDataset
+from PIL import Image
+import numpy as np
 
 class Dataset_handler():
 
@@ -184,3 +187,57 @@ class Dataset_handler():
           os.rename("/content/dataset_depth_ROD/"+str(folder)+"/"+str(img), "/content/dataset_depth_ROD/"+str(folder)+"/"+str(new_name))                     # rename the image (removing the suffix)
     
     return
+  
+  
+  ##Another class which gives us and object of type Dataset containing rotated images
+  
+def Make_rotation(image):
+   val = randint(0, 3)
+
+   # Rotate it by val*90 degrees (0, 90, 180, 270)
+   rotated = np.rot90(image, val)
+   return np.array(rotated).transpose((2, 1, 0)), val
+
+  
+class Rotate(VisionDataset):
+    def __init__(self, dataset,  transform=None):
+        super(Caltech, self).__init__(dataset)
+        self.dataset = dataset
+        self.wrapper = []
+
+        for i in range(len(data_synRODrgb)):
+          image = data_synRODrgb[i][0]
+          img = torchvision.utils.make_grid(image)
+          #print(img.shape)
+          img = img.numpy().transpose((1, 2, 0))
+          #print(img.shape)
+          rotated, label = Make_rotation(img)
+          self.wrapper.append((torch.Tensor(rotated),label))
+          
+    
+    def __getitem__(self, index):
+        '''
+        __getitem__ should access an element through its index
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (sample, target) where target is class_index of the target class.
+        '''
+        image, label = self.wrapper[index]
+                           # Provide a way to access image and label via index
+                           # Image should be a PIL Image but actually is a tensor
+                           # label can be int
+        image = transforms.ToPILImage()(image).convert("RGB")
+        # Applies preprocessing when accessing the image
+        if self.transform is not None:
+            image = self.transform(image)
+
+        return image, label   
+      
+    def __len__(self):
+        '''
+        The __len__ method returns the length of the dataset
+        It is mandatory, as this is used by several other components
+        '''
+        length = len(self.wrapper) # Provide a way to get the length (number of elements) of the dataset
+        return length
